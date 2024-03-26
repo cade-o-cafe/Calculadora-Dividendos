949d0f66563f8b68c3f25d77ec0515f82a0dccfe
Julia Labre <112015111+JuliaLabre@users.noreply.github.com>
2024-03-26 15:12:46 -0300

    Create calculadora.test.js

---
+++ api/src/services/calculadora.test.js
@@ -0,0 +1,16 @@
+// colocar aqui o arquivo que será testado
+
+test("Deve calcular os dividendos corretamente", () => {
+  expect(calcular(1000, 12)).toBe(1000.12);
+  expect(calcular(100, 2)).not.toBe(200);
+});
+
+test("Teste de cálculo com valor negativo", () => {
+Precisa dar erro, o valor não pode ser negativo
+});
+
+test("Teste de calculo com o mês negativo", () => {
+Precisa dar erro, o valor não pode ser negativo
+});
+
+teste de taxa de juros diferente
